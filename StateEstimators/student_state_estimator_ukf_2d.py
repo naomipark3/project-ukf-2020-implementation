39a506d379a68746b13cf6f85848633d63146552
naomipark3 <77819519+naomipark3@users.noreply.github.com>
2023-11-17 14:52:32 -0500

    input time

--- StateEstimators/student_state_estimator_ukf_2d.py
+++ StateEstimators/student_state_estimator_ukf_2d.py
@@ -258,8 +258,8 @@ class UKFStateEstimator2D(object):
     
         self.last_measurement_vector = np.array([tof_height])
     
-        #if self.ready_to_filter:
-        if True:
+        if self.ready_to_filter:
+        # if True:
             print("reaching update input time")
             self.update_input_time(data)
         else:
